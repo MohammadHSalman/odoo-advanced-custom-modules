373513754bc12336b8bbb96de026868d819c9edd
Mohammad Salman <mohammadhsalman@hotmail.com>
2026-05-19 23:14:41 +0400

    Delete sapps_odoo directory

--- sapps_odoo/models/pos_config.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    pos_num = fields.Char(string='Pos Number', store=True, readonly=True, tracking=True)
